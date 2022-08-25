cdcdc20942b226bf74734076bc6864773a50f561
Gildas Montcho <gmontcho@gmail.com>
2022-08-26 00:27:19 +0200

    feat(src/config/database): initialize and connect to database

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+// let's create our own instance of Sequelize
+// param1: app name             param2: database name
+// param3: database password    param4: object
+const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+});
+
+module.exports = sequelize;
