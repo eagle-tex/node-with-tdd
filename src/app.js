68dcb94409d86bad357eb0b9d8bc46c148e3ba75
Gildas Montcho <gmontcho@gmail.com>
2022-08-25 23:07:37 +0200

    feat(src/,package): separate app from server(index) & change start script

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
