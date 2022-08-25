e53fa2115bea4e56f65d3276669a0fb06c74181a
Gildas Montcho <gmontcho@gmail.com>
2022-08-25 23:04:49 +0200

    test(UserRegister): add first test

---
+++ src/__tests__/UserRegister.spec.ts
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('', () => {
+  it('returns 200 OK when signup request is valid', () => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword'
+      })
+      .expect(200);
+  });
+});
