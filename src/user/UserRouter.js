3352118cfff27e3d668c913343204748a6376788
Gildas Montcho <gmontcho@gmail.com>
2022-08-27 00:35:00 +0200

    feat(UserRouter): make test #8 pass - send validationErrors.username

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -6,7 +6,9 @@ const router = express.Router();
 router.post('/api/1.0/users', async (req, res) => {
   const user = req.body;
   if (user.username === null) {
-    return res.status(400).send({ validationErrors: {} });
+    return res
+      .status(400)
+      .send({ validationErrors: { username: 'Username cannot be null' } });
   }
 
   await UserService.save(req.body);
