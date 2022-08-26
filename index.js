1c5fd06c4cc513dfa904ffecf4d59a326f82c51f
Gildas Montcho <gmontcho@gmail.com>
2022-08-26 11:56:08 +0200

    feat(index): persist data from client in database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('./src/app');
+const sequelize = require('./src/config/database');
 
 const port = process.env.PORT || 3000;
+
+sequelize.sync();
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
