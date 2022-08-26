8609e39776e7ffe6b08baf3872d0ab25a136dff4
Gildas Montcho <gmontcho@gmail.com>
2022-08-26 11:55:12 +0200

    feat(src/app): add express.json() middleware to parse json bodies in request

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const User = require('./user/User');
 
 const app = express();
 
+app.use(express.json());
+
 app.post('/api/1.0/users', (req, res) => {
   User.create(req.body).then(() => {
     return res.send({ message: 'User created' });
