936bafcb3f06738f7008752b0d0bc8e70fdb6981
Gildas Montcho <gmontcho@gmail.com>
2022-08-26 10:20:09 +0200

    feat(src/app): add implementation to make test 3 pass

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const User = require('./user/User');
 
 const app = express();
 
-app.post('/api/1.0/users', (_req, res) => {
-  return res.send({ message: 'User created' });
+app.post('/api/1.0/users', (req, res) => {
+  User.create(req.body).then(() => {
+    return res.send({ message: 'User created' });
+  });
 });
 
 module.exports = app;
