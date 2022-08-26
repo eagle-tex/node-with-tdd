0ce45068dd3594a9ab4212d67ed2547b4fbca836
Gildas Montcho <gmontcho@gmail.com>
2022-08-26 10:18:56 +0200

    test(UserRegister): add test 3 (saves user to database) + beforeAll + beforeEach

--- src/__tests__/UserRegister.spec.js
+++ src/__tests__/UserRegister.spec.js
@@ -1,5 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
+const User = require('../user/User');
+const sequelize = require('../config/database');
+
+// we use return to wait for the asynchronous function (sequelize.sync())
+// to resolve before continuing
+beforeAll(() => {
+  return sequelize.sync();
+});
+
+// we use return to wait for the asynchronous function (User.destroy())
+// to resolve before continuing
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe('User Registration', () => {
   it('returns 200 OK when signup request is valid', (done) => {
@@ -27,6 +41,23 @@ describe('User Registration', () => {
       .then((response) => {
         expect(response.body.message).toBe('User created');
         done();
+      });
+  });
+
+  it('saves the user to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword'
+      })
+      .then(() => {
+        // query user table
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
       });
   });
 });
