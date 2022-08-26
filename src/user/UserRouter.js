2d9357944e85fbb020c10e4aab2427bb3f5dd59d
Gildas Montcho <gmontcho@gmail.com>
2022-08-26 13:30:28 +0200

    refactor(UserRouter,UserService): split UserRouter implementation into 2 modules

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const User = require('./User');
-const bcrypt = require('bcrypt');
+const UserService = require('./UserService');
 
 const router = express.Router();
 
-router.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = { ...req.body, password: hash };
-    User.create(user).then(() => {
-      return res.send({ message: 'User created' });
-    });
-  });
+router.post('/api/1.0/users', async (req, res) => {
+  await UserService.save(req.body);
+  return res.send({ message: 'User created' });
 });
 
 module.exports = router;
