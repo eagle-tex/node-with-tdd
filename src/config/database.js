875f886eb50139c6f61e41c261784668242f9d6d
Gildas Montcho <gmontcho@gmail.com>
2022-08-26 23:47:26 +0200

    feat(src/config/database): update database.js to use config based on environment

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,20 @@
 const Sequelize = require('sequelize');
+const config = require('config');
+
+const dbConfig = config.get('database');
 
 // let's create our own instance of Sequelize
 // param1: app name             param2: database name
 // param3: database password    param4: object
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false
-});
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    dialect: dbConfig.dialect,
+    storage: dbConfig.storage,
+    logging: dbConfig.logging
+  }
+);
 
 module.exports = sequelize;
