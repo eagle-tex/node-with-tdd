03bd2c8a601e56751c6c4cb8bad6adc8abcfb3c0
Gildas Montcho <gmontcho@gmail.com>
2022-08-27 13:51:43 +0200

    feat(UserRouter): pass tests #17-23 - update pwd valid. midw w/ bail & regex

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -18,7 +18,17 @@ router.post(
     .bail()
     .isEmail()
     .withMessage('E-mail is not valid'),
-  check('password').notEmpty().withMessage('Password cannot be null'),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters')
+    .bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+    .withMessage(
+      'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'
+    ),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
