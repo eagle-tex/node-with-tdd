9b174e7ae125d014bd0e4b248040860795a165ce
Gildas Montcho <gmontcho@gmail.com>
2022-08-27 10:15:40 +0200

    feat(UserRouter): pass test #12 - add length validator midw with bail [IMPORTANT]

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.post(
   '/api/1.0/users',
-  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4 })
+    .withMessage('Must have min 4 and max 32 characters'),
   check('email').notEmpty().withMessage('E-mail cannot be null'),
   check('password').notEmpty().withMessage('Password cannot be null'),
   async (req, res) => {
@@ -16,7 +21,6 @@ router.post(
       errors.array().forEach((error) => {
         return (validationErrors[error.param] = error.msg);
       });
-      // const response = { validationErrors: { ...req.validationErrors } };
       return res.status(400).send({ validationErrors });
     }
     await UserService.save(req.body);
