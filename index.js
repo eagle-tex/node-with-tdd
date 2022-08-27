a9e97d8adace9616641e5928ac00a49ac4bf567a
Gildas Montcho <gmontcho@gmail.com>
2022-08-27 14:16:27 +0200

    chore(index): change 'port' to 'PORT'

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const sequelize = require('./src/config/database');
 
 const port = process.env.PORT || 3000;
 
-sequelize.sync();
+// { force: true } forces sequelize to sync the db with the latest updates.
+// WARN:NEVER USE THIS IN PRODUCTION - leads to data loss (database erasure)
+sequelize.sync({ force: true });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`App running on PORT ${port}`);
 });
