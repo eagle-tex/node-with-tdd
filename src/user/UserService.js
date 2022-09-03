2cb6e9b4ae23717594c1267d66dcb16bc9f8d13f
Gildas Montcho <gmontcho@gmail.com>
2022-09-03 08:42:26 +0200

    feat(UserService): pass test #44 -> save only required properties (w/o 'inactive')

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -2,8 +2,11 @@ const User = require('./User');
 const bcrypt = require('bcrypt');
 
 const save = async (body) => {
-  const hash = await bcrypt.hash(body.password, 10);
-  const user = { ...body, password: hash };
+  // destructure body (req.body) and get the specific fields we need
+  // username, email, password fields are retained, other fields are NOT
+  const { username, email, password } = body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = { username, email, password: hash };
   await User.create(user);
 };
 
