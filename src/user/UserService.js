277e71a61f263f140bc82e103ab3eb53f6445334
Gildas Montcho <gmontcho@gmail.com>
2022-09-03 08:52:05 +0200

    feat(UserService): pass test #45 -> create generateToken func & save token in db

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -1,12 +1,22 @@
 const User = require('./User');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+
+const generateToken = (length) => {
+  return crypto.randomBytes(length).toString('hex').substring(0, length);
+};
 
 const save = async (body) => {
   // destructure body (req.body) and get the specific fields we need
   // username, email, password fields are retained, other fields are NOT
   const { username, email, password } = body;
   const hash = await bcrypt.hash(password, 10);
-  const user = { username, email, password: hash };
+  const user = {
+    username,
+    email,
+    password: hash,
+    activationToken: generateToken(16)
+  };
   await User.create(user);
 };
 
