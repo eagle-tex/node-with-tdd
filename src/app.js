7bfbd7bc0daa6d11c167e6363e2164ea928a8679
Gildas Montcho <gmontcho@gmail.com>
2022-09-04 18:39:23 +0200

    feat(app): update error handler to handle validationErrors

--- src/app.js
+++ src/app.js
@@ -29,8 +29,17 @@ app.use(express.json());
 app.use(UserRouter);
 
 app.use((err, req, res, _next) => {
-  const { status, message } = err;
-  res.status(status).send({ message: req.t(message) });
+  const { status, message, errors } = err;
+  let validationErrors = null;
+
+  if (errors) {
+    validationErrors = {};
+    errors.forEach((error) => {
+      return (validationErrors[error.param] = req.t(error.msg));
+    });
+  }
+
+  res.status(status).send({ message: req.t(message), validationErrors });
 });
 
 module.exports = app;
