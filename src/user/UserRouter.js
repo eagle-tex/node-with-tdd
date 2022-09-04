3689020ea4b64d7601ebf90b3fd085718b8e813a
Gildas Montcho <gmontcho@gmail.com>
2022-09-04 12:07:21 +0200

    feat(UserRouter): pass #55-56 -> send err.message in catch block on activation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -57,7 +57,7 @@ router.post('/api/1.0/users/token/:token', async (req, res) => {
   try {
     await UserService.activate(token);
   } catch (err) {
-    return res.status(400).send();
+    return res.status(400).send({ message: req.t(err.message) });
   }
   res.send();
 });
