7929932e927493ba6581142233787562c25f4e4f
Gildas Montcho <gmontcho@gmail.com>
2022-09-04 17:52:09 +0200

    style(EmailService): fix indentation in html code (manually)

--- src/email/EmailService.js
+++ src/email/EmailService.js
@@ -9,13 +9,14 @@ const sendAccountActivation = async (email, token) => {
     to: email,
     subject: 'Account Activation',
     html: `
-<div>
-<b>Please click below link to activate your account</b>
-</div>
-<div>
-<a href="http://localhost:8080/#/login?token=${token}">Activate
-</a>
-</div>
+      <div>
+        <b>Please click below link to activate your account</b>
+      </div>
+      <div>
+        <a href="http://localhost:8080/#/login?token=${token}">
+          Activate
+        </a>
+      </div>
 `
   });
 
