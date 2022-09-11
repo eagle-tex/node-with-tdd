b576f949cf83afa537548ceca37e5ab284058593
Gildas Montcho <gmontcho@gmail.com>
2022-09-11 23:04:15 +0200

    refactor(Listing): add functions getUsers & addUsers to make tests cleaner

--- src/__tests__/UserListing.spec.js
+++ src/__tests__/UserListing.spec.js
@@ -11,14 +11,27 @@ beforeEach(() => {
   return User.destroy({ truncate: true });
 });
 
+const getUsers = () => {
+  return request(app).get('/api/1.0/users');
+};
+
+const addUsers = async (count) => {
+  for (let i = 0; i < count; i++) {
+    await User.create({
+      username: `user${i + 1}`,
+      email: `user${i + 1}@mail.com`
+    });
+  }
+};
+
 describe('Listing Users', () => {
   it('returns 200 OK when there are no user in database', async () => {
-    const response = await request(app).get('/api/1.0/users');
+    const response = await getUsers();
     expect(response.status).toBe(200);
   });
 
   it('returns page object as response body', async () => {
-    const response = await request(app).get('/api/1.0/users');
+    const response = await getUsers();
     expect(response.body).toEqual({
       content: [],
       page: 0,
@@ -28,14 +41,8 @@ describe('Listing Users', () => {
   });
 
   it('returns 10 users in page content when there all 11 users in database', async () => {
-    for (let i = 0; i < 11; i++) {
-      await User.create({
-        username: `user${i + 1}`,
-        email: `user${i + 1}@mail.com`
-      });
-    }
-
-    const response = await request(app).get('/api/1.0/users');
+    await addUsers(11);
+    const response = await getUsers();
 
     expect(response.body.content.length).toBe(10);
   });
