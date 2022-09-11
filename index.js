838dccff76fa939df3ba9496a2b18adb78d22ed1
Gildas Montcho <gmontcho@gmail.com>
2022-09-12 01:06:21 +0200

    feat(index): add addUsers to index.js to test application on client

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
+const User = require('./src/user/User');
 
 const port = process.env.PORT || 3000;
 
+const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+  for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
+    await User.create({
+      username: `user${i + 1}`,
+      email: `user${i + 1}@mail.com`,
+      inactive: i >= activeUserCount
+    });
+  }
+};
+
 // { force: true } forces sequelize to sync the db with the latest updates.
 // WARN:NEVER USE THIS IN PRODUCTION - leads to data loss (database erasure)
-sequelize.sync({ force: true });
+sequelize.sync({ force: true }).then(async () => {
+  await addUsers(25);
+});
 
 app.listen(port, () => {
   console.log(`App running on PORT ${port}`);
