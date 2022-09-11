aae31d0f8bf4514ac1ee4bf4d75d91ea747b60bb
Gildas Montcho <gmontcho@gmail.com>
2022-09-11 22:41:26 +0200

    refactor(Router): use getUsers funciton in get route handler

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -59,13 +59,9 @@ router.post('/api/1.0/users/token/:token', async (req, res, next) => {
   }
 });
 
-router.get('/api/1.0/users', (_req, res) => {
-  res.send({
-    content: [],
-    page: 0,
-    size: 10,
-    totalPages: 0
-  });
+router.get('/api/1.0/users', async (_req, res) => {
+  const users = await UserService.getUsers();
+  res.send(users);
 });
 
 module.exports = router;
