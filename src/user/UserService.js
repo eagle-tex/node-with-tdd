6993ccc7dbfffdda82114115478fea034ba75d3d
Gildas Montcho <gmontcho@gmail.com>
2022-09-12 00:46:55 +0200

    feat(Service): pass #71 in isolation -> add parameter page to getUsers function

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -53,19 +53,20 @@ const activate = async (token) => {
   await user.save();
 };
 
-const getUsers = async () => {
+const getUsers = async (page) => {
   const pageSize = 10;
   const usersWithCount = await User.findAndCountAll({
     where: { inactive: false },
     attributes: ['id', 'username', 'email'],
-    limit: pageSize
+    limit: pageSize,
+    offset: page * pageSize
   });
 
   console.log(usersWithCount);
 
   return {
     content: usersWithCount.rows,
-    page: 0,
+    page: Number.parseInt(page),
     size: 10,
     totalPages: Math.ceil(usersWithCount.count / pageSize)
   };
