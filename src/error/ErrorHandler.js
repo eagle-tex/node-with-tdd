1fcaf00e19b4f574dad1ec7e0be714933f28c503
Gildas Montcho <gmontcho@gmail.com>
2022-09-13 06:59:11 +0200

    fix(ErrorHandler): pass #80 -> set validationErrors=undefined on declar., NOT null

--- src/error/ErrorHandler.js
+++ src/error/ErrorHandler.js
@@ -1,6 +1,6 @@
 module.exports = (err, req, res, _next) => {
   const { status, message, errors } = err;
-  let validationErrors = null;
+  let validationErrors;
 
   if (errors) {
     validationErrors = {};
