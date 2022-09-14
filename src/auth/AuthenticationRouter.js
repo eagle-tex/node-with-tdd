fe7e831da234f31b1dff0de0580a69b5ec80fd15
Gildas Montcho <gmontcho@gmail.com>
2022-09-14 07:45:55 +0200

    feat(AuthRouter): pass #95 -> validate email field w/ middleware before handling req

--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -1,32 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
 
 const UserService = require('../user/UserService');
 const AuthenticationException = require('./AuthenticationException');
 const ForbiddenException = require('./ForbiddenException');
 
-router.post('/api/1.0/auth', async (req, res, next) => {
-  const { email, password } = req.body;
-  const user = await UserService.findByEmail(email);
-  if (!user) {
-    return next(new AuthenticationException());
-  }
+router.post(
+  '/api/1.0/auth',
+  check('email').isEmail(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(new AuthenticationException());
+    }
 
-  // bcrypt(clear_password, hashed_password_in_user_table)
-  const match = await bcrypt.compare(password, user.password);
-  if (!match) {
-    return next(new AuthenticationException());
-  }
+    const { email, password } = req.body;
+    const user = await UserService.findByEmail(email);
+    if (!user) {
+      return next(new AuthenticationException());
+    }
 
-  if (user.inactive) {
-    return next(new ForbiddenException());
-  }
+    // bcrypt(clear_password, hashed_password_in_user_table)
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return next(new AuthenticationException());
+    }
 
-  res.send({
-    id: user.id,
-    username: user.username
-  });
-});
+    if (user.inactive) {
+      return next(new ForbiddenException());
+    }
+
+    res.send({
+      id: user.id,
+      username: user.username
+    });
+  }
+);
 
 module.exports = router;
