c2a63e81939e22c0233483368c198b843527c002
Gildas Montcho <gmontcho@gmail.com>
2022-09-14 22:39:37 +0200

    test(Update): add #98-99 'ret. error body w/ transl. msg for unauthorized request'

--- src/__tests__/UserUpdate.spec.js
+++ src/__tests__/UserUpdate.spec.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../app');
 const User = require('../user/User');
 const sequelize = require('../config/database');
-const bcrypt = require('bcrypt');
-// const en = require('../../locales/en/translation.json');
-// const fr = require('../../locales/fr/translation.json');
+// const bcrypt = require('bcrypt');
+const en = require('../../locales/en/translation.json');
+const fr = require('../../locales/fr/translation.json');
 
 beforeAll(async () => {
   await sequelize.sync();
@@ -20,4 +20,23 @@ describe('User Update', () => {
 
     expect(response.status).toBe(403);
   });
+
+  it.each`
+    language | message
+    ${'en'}  | ${en.unauthorized_user_update}
+    ${'fr'}  | ${fr.unauthorized_user_update}
+  `(
+    'returns error body with "$message" for unauthorized request when language is $language',
+    async ({ language, message }) => {
+      const nowInMillis = new Date().getTime();
+      const response = await request(app)
+        .put('/api/1.0/users/5')
+        .set('Accept-Language', language)
+        .send();
+
+      expect(response.body.path).toBe('/api/1.0/users/5');
+      expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
+      expect(response.body.message).toBe(message);
+    }
+  );
 });
