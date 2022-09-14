f42f72f582c52ac79f24c9822e2b1c73f0882319
Gildas Montcho <gmontcho@gmail.com>
2022-09-14 07:16:38 +0200

    test(Auth): add #90 'returns 401 when password is wrong'

--- src/__tests__/Auth.spec.js
+++ src/__tests__/Auth.spec.js
@@ -97,4 +97,14 @@ describe('Authentication', () => {
       expect(response.body.message).toBe(message);
     }
   );
+
+  it('returns 401 when password is wrong', async () => {
+    await addUser();
+    const response = await postAuthentication({
+      email: 'user1@mail.com',
+      password: 'Inc0rrect-password'
+    });
+
+    expect(response.status).toBe(401);
+  });
 });
