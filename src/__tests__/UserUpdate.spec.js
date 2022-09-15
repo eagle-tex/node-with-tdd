666dfab463258de5dea18955a06f26bdacc64e7b
Gildas Montcho <gmontcho@gmail.com>
2022-09-15 05:46:34 +0200

    refactor(Update): create putUser function and apply DRY principle in tests w/ it

--- src/__tests__/UserUpdate.spec.js
+++ src/__tests__/UserUpdate.spec.js
@@ -14,9 +14,19 @@ beforeEach(async () => {
   await User.destroy({ truncate: true });
 });
 
+const putUser = (id = 5, body = null, options = {}) => {
+  const agent = request(app).put(`/api/1.0/users/${id}`);
+
+  if (options.language) {
+    agent.set('Accept-Language', options.language);
+  }
+
+  return agent.send(body);
+};
+
 describe('User Update', () => {
   it('returns 403 Forbidden when request sent without basic authorization', async () => {
-    const response = await request(app).put('/api/1.0/users/5').send();
+    const response = await putUser();
 
     expect(response.status).toBe(403);
   });
@@ -29,10 +39,7 @@ describe('User Update', () => {
     'returns error body with "$message" for unauthorized request when language is $language',
     async ({ language, message }) => {
       const nowInMillis = new Date().getTime();
-      const response = await request(app)
-        .put('/api/1.0/users/5')
-        .set('Accept-Language', language)
-        .send();
+      const response = await putUser(5, null, { language });
 
       expect(response.body.path).toBe('/api/1.0/users/5');
       expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
