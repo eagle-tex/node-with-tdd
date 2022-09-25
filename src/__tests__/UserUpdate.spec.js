8428aea70a0f5b02eeb14e8d2d6c2765b3525bc8
Gildas Montcho <gmontcho@gmail.com>
2022-09-25 17:40:19 +0200

    test(Update): add #108 'ret. 403 when token is not valid' & update putUser

--- src/__tests__/UserUpdate.spec.js
+++ src/__tests__/UserUpdate.spec.js
@@ -47,6 +47,10 @@ const putUser = async (id = 5, body = null, options = {}) => {
     agent.set('Authorization', `Bearer ${token}`);
   }
 
+  if (options.token) {
+    agent.set('Authorization', `Bearer ${options.token}`);
+  }
+
   return agent.send(body);
 };
 
@@ -135,5 +139,11 @@ describe('User Update', () => {
     const inDBUser = await User.findOne({ where: { id: savedUser.id } });
 
     expect(inDBUser.username).toBe(validUpdate.username);
+  });
+
+  it('returns 403 when token is not valid', async () => {
+    const response = await putUser(5, null, { token: '123' });
+
+    expect(response.status).toBe(403);
   });
 });
