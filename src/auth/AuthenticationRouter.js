3ffbbe5ce584e59d2945b630252b9c5c50c1f449
Gildas Montcho <gmontcho@gmail.com>
2022-09-25 09:48:07 +0200

    refactor(AuthRouter): use TokenService.createToken function

--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const { check, validationResult } = require('express-validator');
-const jwt = require('jsonwebtoken');
 
 const UserService = require('../user/UserService');
 const AuthenticationException = require('./AuthenticationException');
 const ForbiddenException = require('../error/ForbiddenException');
+const TokenService = require('./TokenService');
 
 router.post(
   '/api/1.0/auth',
@@ -33,7 +33,7 @@ router.post(
       return next(new ForbiddenException());
     }
 
-    const token = jwt.sign({ id: user.id }, 'this-is-our-secret');
+    const token = TokenService.createToken(user);
 
     res.send({
       id: user.id,
