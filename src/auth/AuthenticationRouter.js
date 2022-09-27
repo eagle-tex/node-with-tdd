3dd659b6714f5ffea9923a4c4ede630d86865b4d
Gildas Montcho <gmontcho@gmail.com>
2022-09-27 06:45:59 +0200

    feat(AuthRouter): pass #110 -> update logout route handler to delete token on logout

--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -43,7 +43,13 @@ router.post(
   }
 );
 
-router.post('/api/1.0/logout', (req, res) => {
+router.post('/api/1.0/logout', async (req, res) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.substring(7);
+    await TokenService.deleteToken(token);
+  }
+
   res.send();
 });
 
