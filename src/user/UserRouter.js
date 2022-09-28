d81117e0b1820669f98ea2bc961a4bc2908fca18
Gildas Montcho <gmontcho@gmail.com>
2022-09-28 06:01:17 +0200

    feat(UserRouter): pass #118 -> delete user token from db after user deletion

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -6,6 +6,7 @@ const ValidationException = require('../error/ValidationException');
 const pagination = require('../middleware/pagination');
 const ForbiddenException = require('../error/ForbiddenException');
 const tokenAuthentication = require('../middleware/tokenAuthentication');
+const TokenService = require('../auth/TokenService');
 
 const router = express.Router();
 
@@ -119,6 +120,10 @@ router.delete(
     }
 
     await UserService.deleteUser(req.params.id);
+    const authorization = req.headers.authorization;
+    const token = authorization.substring(7);
+    await TokenService.deleteToken(token);
+
     res.send();
   }
 );
