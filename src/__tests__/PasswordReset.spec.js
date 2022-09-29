968beab2555ab76e8f41fff336aaeaea0838a8dd
Gildas Montcho <gmontcho@gmail.com>
2022-09-29 22:54:02 +0200

    test(PwdReset): add #128 & imports & beforeAll & beforeEach & addUser helper func

--- src/__tests__/PasswordReset.spec.js
+++ src/__tests__/PasswordReset.spec.js
@@ -1,8 +1,37 @@
 const request = require('supertest');
+const bcrypt = require('bcrypt');
 
 const app = require('../app');
 const en = require('../../locales/en/translation.json');
 const fr = require('../../locales/fr/translation.json');
+const User = require('../user/User');
+const sequelize = require('../config/database');
+
+beforeAll(async () => {
+  await sequelize.sync();
+});
+
+beforeEach(async () => {
+  // NOTE: because we included `userId` field as a foreignKey in User-Token
+  // relationship, the `{ truncate: true }` option would not be valid anymore
+  // the database will not allow a `{ truncate: true }`.
+  // we replace that with a `{ truncate: { cascade: true }}`
+  await User.destroy({ truncate: { cascade: true } });
+});
+
+const activeUser = {
+  username: 'user1',
+  email: 'user1@mail.com',
+  password: 'P4ssword',
+  inactive: false
+};
+
+const addUser = async (user = { ...activeUser }) => {
+  const hash = await bcrypt.hash(user.password, 10);
+  user.password = hash;
+
+  return await User.create(user); // return the created user object
+};
 
 const postPasswordReset = (email = 'user1@mail.com', options = {}) => {
   const agent = request(app).post('/api/1.0/password-reset');
@@ -54,4 +83,11 @@ describe('Password Reset', () => {
       expect(response.status).toBe(400);
     }
   );
+
+  it('returns 200 OK when a password resest request is sent for known e-mail', async () => {
+    const user = await addUser();
+    const response = await postPasswordReset(user.email);
+
+    expect(response.status).toBe(200);
+  });
 });
