a71770ea466979c65f645e7309824f738d58e6f1
Gildas Montcho <gmontcho@gmail.com>
2022-09-29 22:24:26 +0200

    test(PwdReset): add #124-125 'ret. error body w/ transl. msg for unknown email'

--- src/__tests__/PasswordReset.spec.js
+++ src/__tests__/PasswordReset.spec.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 
 const app = require('../app');
+const en = require('../../locales/en/translation.json');
+const fr = require('../../locales/fr/translation.json');
 
 describe('Password Reset', () => {
   it('returns 404 when a password reset request is sent for unknown e-mail', async () => {
@@ -10,4 +12,23 @@ describe('Password Reset', () => {
 
     expect(response.status).toBe(404);
   });
+
+  it.each`
+    language | message
+    ${'en'}  | ${en.email_not_in_use}
+    ${'fr'}  | ${fr.email_not_in_use}
+  `(
+    'returns error body with "$message" for unknown e-mail for password reset request when language is $language',
+    async ({ language, message }) => {
+      const nowInMillis = new Date().getTime();
+      const response = await request(app)
+        .post('/api/1.0/password-reset')
+        .set('Accept-Language', language)
+        .send({ email: 'user1@mail.com' });
+
+      expect(response.body.path).toBe('/api/1.0/password-reset');
+      expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
+      expect(response.body.message).toBe(message);
+    }
+  );
 });
