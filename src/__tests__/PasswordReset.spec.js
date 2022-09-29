eaa3f48b0bc84e06d5960cc498d27961c844823b
Gildas Montcho <gmontcho@gmail.com>
2022-09-29 22:10:52 +0200

    test(PwdReset): add #124 'ret. 404 when pwd reset req sent for unknown e-mail'

--- src/__tests__/PasswordReset.spec.js
+++ src/__tests__/PasswordReset.spec.js
@@ -3,7 +3,11 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Password Reset', () => {
-  it('fails', () => {
-    expect(1).toBe(2);
+  it('returns 404 when a password reset request is sent for unknown e-mail', async () => {
+    const response = await request(app)
+      .post('/api/1.0/password-reset')
+      .send({ email: 'user1@mail.com' });
+
+    expect(response.status).toBe(404);
   });
 });
