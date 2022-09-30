f79c58cbba1dd8ab5dc39f52855e483d6ecaf177
Gildas Montcho <gmontcho@gmail.com>
2022-09-30 07:17:55 +0200

    feat(UserService): pass #133 -> wrap sendPwdRst in try & throw EmailException on err

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -104,7 +104,12 @@ const passwordResetRequest = async (email) => {
 
   user.passwordResetToken = randomString(16);
   await user.save();
-  await EmailService.sendPasswordReset(email, user.passwordResetToken);
+
+  try {
+    await EmailService.sendPasswordReset(email, user.passwordResetToken);
+  } catch (err) {
+    throw new EmailException();
+  }
 };
 
 module.exports = {
