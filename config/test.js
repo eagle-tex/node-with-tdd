0a29c12422b73d3d8294edda9698e9791c1af13e
Gildas Montcho <gmontcho@gmail.com>
2022-09-30 07:09:51 +0200

    feat(config/): fix broken tests in UserRegister -> rename config json files to js

---
+++ config/test.js
@@ -0,0 +1,15 @@
+module.exports = {
+  database: {
+    database: 'hoaxify',
+    username: 'my-db-user',
+    password: 'db-p4ss',
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false
+  },
+  mail: {
+    host: 'localhost',
+    port: Math.floor(Math.random() * 2000) + 10000, // random port between 10000 & 12000
+    tls: { rejectUnauthorized: false }
+  }
+};
