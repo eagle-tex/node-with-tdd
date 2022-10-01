8c8202b96dcf349e85d65bf0c67333f710aaf4a0
Gildas Montcho <gmontcho@gmail.com>
2022-10-01 21:49:13 +0200

    feat(UserRouter): pass #163 -> return user on success in PUT user route handler

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -90,9 +90,9 @@ router.put('/api/1.0/users/:id', async (req, res, next) => {
     return next(new ForbiddenException('unauthorized_user_update'));
   }
 
-  await UserService.updateUser(req.params.id, req.body);
+  const user = await UserService.updateUser(req.params.id, req.body);
 
-  return res.send();
+  return res.send(user);
 });
 
 router.delete('/api/1.0/users/:id', async (req, res, next) => {
