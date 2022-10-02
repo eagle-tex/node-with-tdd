fc933246cb702ef74c4e39c9c3b80789de44f32e
Gildas Montcho <gmontcho@gmail.com>
2022-10-02 17:19:28 +0200

    feat(FService): pass #165 -> in createFolders func, create `profileFolder`

--- src/file/FileService.js
+++ src/file/FileService.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
 const createFolders = () => {
   const uploadDir = 'upload';
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync('upload');
+  }
+
+  const profileFolder = path.join('.', 'upload', 'profile');
+  if (!fs.existsSync(profileFolder)) {
+    fs.mkdirSync(profileFolder);
   }
 };
 
