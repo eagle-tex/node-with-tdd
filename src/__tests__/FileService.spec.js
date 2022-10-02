dfac4dec3e0d475fdc15c3621c3d25754135b939
Gildas Montcho <gmontcho@gmail.com>
2022-10-02 21:54:36 +0200

    test(FService): #164-165 - update tests to use config files properties

--- src/__tests__/FileService.spec.js
+++ src/__tests__/FileService.spec.js
@@ -1,21 +1,23 @@
 const FileService = require('../file/FileService');
 const fs = require('fs');
 const path = require('path');
+const config = require('config');
+
+const { uploadDir, profileDir } = config;
 
 describe('createFolders', () => {
   it('creates upload folder', () => {
     FileService.createFolders();
-    const folderName = 'upload';
 
-    expect(fs.existsSync(folderName)).toBe(true);
+    expect(fs.existsSync(uploadDir)).toBe(true);
   });
 
   it('creates profile folder under upload folder', () => {
     FileService.createFolders();
-    const testFolder = path.join(__dirname, '..', '..', 'upload', 'profile');
-    console.log({ testFolder });
+    // const testFolder = path.join(__dirname, '..', '..', 'upload', 'profile');
+    // console.log({ testFolder });
 
-    const profileFolder = path.join('.', 'upload', 'profile');
+    const profileFolder = path.join('.', uploadDir, profileDir);
     console.log({ profileFolder });
 
     expect(fs.existsSync(profileFolder)).toBe(true);
