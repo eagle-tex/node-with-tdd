ba37a9c627bc6fd3c47cd297d9137731f414a67e
Gildas Montcho <gmontcho@gmail.com>
2022-10-03 07:30:57 +0200

    test(Static): add #167 'returns 404 when file not found'

--- src/__tests__/ServingStaticResources.spec.js
+++ src/__tests__/ServingStaticResources.spec.js
@@ -1,8 +1,10 @@
-// const request = require('supertest');
-// const app = require('../app');
+const request = require('supertest');
+const app = require('../app');
 
 describe('Profile Images', () => {
-  it('returns 404 when file not found', () => {
-    expect(1).toBe(2);
+  it('returns 404 when file not found', async () => {
+    const response = await request(app).get('/images/123456');
+
+    expect(response.status).toBe(404);
   });
 });
