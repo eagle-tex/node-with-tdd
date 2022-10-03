0216db9806bbadc925b64da8182c073e94e9aad4
Gildas Montcho <gmontcho@gmail.com>
2022-10-03 07:27:33 +0200

    test(Static): 'Profile Images'/ add describe block 'Profile Image'

---
+++ src/__tests__/ServingStaticResources.spec.js
@@ -0,0 +1,8 @@
+// const request = require('supertest');
+// const app = require('../app');
+
+describe('Profile Images', () => {
+  it('returns 404 when file not found', () => {
+    expect(1).toBe(2);
+  });
+});
