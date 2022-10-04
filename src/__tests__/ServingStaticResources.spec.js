243901a69278345c266255467bd99756d7972637
Gildas Montcho <gmontcho@gmail.com>
2022-10-04 06:26:42 +0200

    refactor(Static): add copyFile helper func & use it to DRY our tests

--- src/__tests__/ServingStaticResources.spec.js
+++ src/__tests__/ServingStaticResources.spec.js
@@ -8,6 +8,14 @@ const { uploadDir, profileDir } = config;
 const profileFolder = path.join('.', uploadDir, profileDir);
 
 describe('Profile Images', () => {
+  const copyFile = () => {
+    const filePath = path.join(__dirname, 'resources', 'test-png.png');
+    const storedFileName = 'test-file';
+    const targetPath = path.join(profileFolder, storedFileName);
+    fs.copyFileSync(filePath, targetPath);
+    return storedFileName;
+  };
+
   it('returns 404 when file not found', async () => {
     const response = await request(app).get('/images/123456');
 
@@ -15,20 +23,14 @@ describe('Profile Images', () => {
   });
 
   it('returns 200 OK when file exists', async () => {
-    const filePath = path.join(__dirname, 'resources', 'test-png.png');
-    const storedFileName = 'test-file';
-    const targetPath = path.join(profileFolder, storedFileName);
-    fs.copyFileSync(filePath, targetPath);
+    const storedFileName = copyFile();
     const response = await request(app).get(`/images/${storedFileName}`);
 
     expect(response.status).toBe(200);
   });
 
   it('returns cache for 1 year in response', async () => {
-    const filePath = path.join(__dirname, 'resources', 'test-png.png');
-    const storedFileName = 'test-file';
-    const targetPath = path.join(profileFolder, storedFileName);
-    fs.copyFileSync(filePath, targetPath);
+    const storedFileName = copyFile();
     const response = await request(app).get(`/images/${storedFileName}`);
     const oneYearInSeconds = 365 * 24 * 60 * 60;
 
