ee60587639d8ee7a38e5d73f8bbabbcb8200acfe
Gildas Montcho <gmontcho@gmail.com>
2022-10-04 21:58:22 +0200

    test(UUpdate): add #180 'ret. transl. msg when file size > 2MB'

--- src/__tests__/UserUpdate.spec.js
+++ src/__tests__/UserUpdate.spec.js
@@ -302,4 +302,24 @@ describe('User Update', () => {
     const userInDB = await User.findOne({ where: { id: savedUser.id } });
     expect(userInDB.image).toBe(firstImage);
   });
+
+  it.each`
+    language | message
+    ${'en'}  | ${en.profile_image_size}
+    ${'fr'}  | ${fr.profile_image_size}
+  `(
+    'returns "$message" when file size exceeds 2 MB when language is $language',
+    async ({ language, message }) => {
+      const fileExceeding2MB = 'a'.repeat(2 * 1024 * 1024) + 'a';
+      const base64 = Buffer.from(fileExceeding2MB).toString('base64');
+      const savedUser = await addUser();
+      const invalidUpdate = { username: 'updated-user', image: base64 };
+      const response = await putUser(savedUser.id, invalidUpdate, {
+        auth: { email: savedUser.email, password: 'P4ssword' },
+        language
+      });
+
+      expect(response.body.validationErrors.image).toBe(message);
+    }
+  );
 });
