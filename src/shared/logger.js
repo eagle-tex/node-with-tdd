6adaaa07ab3dd3ec6c31f54367cdf4324f1dba2f
Gildas Montcho <gmontcho@gmail.com>
2022-10-08 09:24:06 +0200

    feat(logger.js): setup logger (draft 2)

--- src/shared/logger.js
+++ src/shared/logger.js
@@ -1,11 +1,20 @@
-const { createLogger, transports } = require('winston');
+const { createLogger, transports, format } = require('winston');
 
+const customFormat = format.combine(
+  format.timestamp(),
+  format.printf((info) => {
+    return `${info.timestamp} [${info.level.toUpperCase().padEnd(7)}] : ${
+      info.message
+    }`;
+  })
+);
 const logger = createLogger({
   transports: [
     new transports.Console({ level: 'silly' }),
     new transports.File({ filename: 'app.log', level: 'error' })
   ],
-  level: 'debug'
+  level: 'debug',
+  format: customFormat
 });
 
 module.exports = logger;
