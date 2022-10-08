2a0ba88c342e8122aad7b57e5806677a8e675b1c
Gildas Montcho <gmontcho@gmail.com>
2022-10-08 07:23:46 +0200

    feat(database/config.js): generate the database configs for each env w/ console.log

--- database/config.js
+++ database/config.js
@@ -1,18 +1,30 @@
-module.exports = {
-  development: {
-    username: 'my-db-user',
-    password: 'db-p4ss',
-    database: 'hoaxify',
-    host: 'localhost',
-    dialect: 'sqlite',
-    storage: './database.sqlite'
-  },
-  staging: {
-    username: 'my-db-user',
-    password: 'db-p4ss',
-    database: 'hoaxify',
-    host: 'localhost',
-    dialect: 'sqlite',
-    storage: './staging.sqlite'
-  }
-};
+const profiles = require('../config');
+
+const dbConfigs = {};
+
+Object.keys(profiles).forEach((profile) => {
+  dbConfigs[profile] = { ...profiles[profile].database };
+});
+
+console.log(dbConfigs);
+
+module.exports = dbConfigs;
+
+// module.exports = {
+//   development: {
+//     username: 'my-db-user',
+//     password: 'db-p4ss',
+//     database: 'hoaxify',
+//     host: 'localhost',
+//     dialect: 'sqlite',
+//     storage: './database.sqlite'
+//   },
+//   staging: {
+//     username: 'my-db-user',
+//     password: 'db-p4ss',
+//     database: 'hoaxify',
+//     host: 'localhost',
+//     dialect: 'sqlite',
+//     storage: './staging.sqlite'
+//   }
+// };
