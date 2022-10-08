0bf7c4b7424427e70d36f9471c6012168835a83f
Gildas Montcho <gmontcho@gmail.com>
2022-10-08 07:28:20 +0200

    feat(database/config.js): remove comments & console.log

--- database/config.js
+++ database/config.js
@@ -6,35 +6,4 @@ Object.keys(profiles).forEach((profile) => {
   dbConfigs[profile] = { ...profiles[profile].database };
 });
 
-console.log(dbConfigs);
-
 module.exports = dbConfigs;
-
-// running the command `node config` from the `./database/` folder
-// produces the following output
-// {
-//   development: {
-//     database: 'hoaxify',
-//     username: 'my-db-user',
-//     password: 'db-p4ss',
-//     dialect: 'sqlite',
-//     storage: './database.sqlite',
-//     logging: false
-//   },
-//   staging: {
-//     database: 'hoaxify',
-//     username: 'my-db-user',
-//     password: 'db-p4ss',
-//     dialect: 'sqlite',
-//     storage: './staging.sqlite',
-//     logging: false
-//   },
-//   test: {
-//     database: 'hoaxify',
-//     username: 'my-db-user',
-//     password: 'db-p4ss',
-//     dialect: 'sqlite',
-//     storage: ':memory:',
-//     logging: false
-//   }
-// }
