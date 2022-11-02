55098da64c8918e84c47426a9e3f53abfec9663e
Gildas Montcho <gmontcho@gmail.com>
2022-11-02 23:46:44 +0100

    test(HoaxSubmit): add #193-194 'ret. error body w/ transl. msg on unauthor request'

--- __tests__/HoaxSubmit.spec.js
+++ __tests__/HoaxSubmit.spec.js
@@ -1,8 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-const postHoax = (body) => {
-  return request(app).post('/api/1.0/hoaxes').send(body);
+const en = require('../locales/en/translation.json');
+const fr = require('../locales/fr/translation.json');
+
+const postHoax = (body, options = {}) => {
+  const agent = request(app).post('/api/1.0/hoaxes');
+
+  if (options.language) {
+    agent.set('Accept-Language', options.language);
+  }
+
+  return agent.send(body);
 };
 
 describe('Post Hoax', () => {
@@ -10,4 +19,21 @@ describe('Post Hoax', () => {
     const response = await postHoax();
     expect(response.status).toBe(401);
   });
+
+  it.each`
+    language | message
+    ${'en'}  | ${en.unauthorized_hoax_submit}
+    ${'fr'}  | ${fr.unauthorized_hoax_submit}
+  `(
+    `returns error body with "$message" when unauthorized request sent with language $language`,
+    async ({ language, message }) => {
+      const nowInMillis = Date.now();
+      const response = await postHoax(null, { language });
+      const error = response.body;
+
+      expect(error.path).toBe('/api/1.0/hoaxes');
+      expect(error.message).toBe(message);
+      expect(error.timestamp).toBeGreaterThan(nowInMillis);
+    }
+  );
 });
