0c06dab7f41310ddd714d122f9f5705d020bec6e
Gildas Montcho <gmontcho@gmail.com>
2022-11-03 00:01:29 +0100

    feat(HoaxRouter): pass #195 -> update POST hoax route handler w/ tokenAuth middlew

--- src/hoax/HoaxRouter.js
+++ src/hoax/HoaxRouter.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const AuthenticationException = require('../auth/AuthenticationException');
 
-router.post('/api/1.0/hoaxes', (_req, _res) => {
+router.post('/api/1.0/hoaxes', (req, res) => {
+  if (req.authenticatedUser) {
+    return res.send();
+  }
+
   throw new AuthenticationException('unauthorized_hoax_submit');
 });
 
