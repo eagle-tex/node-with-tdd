2fb9cb5fc73079373d88b4cfee80f49e39dc4201
Gildas Montcho <gmontcho@gmail.com>
2022-11-03 14:48:25 +0100

    feat(HoaxRouter): pass #202-203 -> add validation to POST hoax rte hdler & chge ifs order

--- src/hoax/HoaxRouter.js
+++ src/hoax/HoaxRouter.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 const AuthenticationException = require('../auth/AuthenticationException');
 const HoaxService = require('./HoaxService');
+const ValidationException = require('../error/ValidationException');
 
-router.post('/api/1.0/hoaxes', async (req, res, next) => {
-  if (req.authenticatedUser) {
+router.post(
+  '/api/1.0/hoaxes',
+  check('content').isLength({ min: 10 }),
+  async (req, res, next) => {
+    if (!req.authenticatedUser) {
+      next(new AuthenticationException('unauthorized_hoax_submit'));
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(new ValidationException(errors.array()));
+    }
+
     await HoaxService.save(req.body);
     return res.send({ message: req.t('hoax_submit_success') });
   }
-
-  next(new AuthenticationException('unauthorized_hoax_submit'));
-});
+);
 
 module.exports = router;
