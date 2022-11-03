e74bc949c2b8bc15f00fec96dfea7c67a9aad639
Gildas Montcho <gmontcho@gmail.com>
2022-11-03 01:33:55 +0100

    feat(HoaxRouter): pass #200-201 -> return response w/ translated key as message

--- src/hoax/HoaxRouter.js
+++ src/hoax/HoaxRouter.js
@@ -6,7 +6,7 @@ const HoaxService = require('./HoaxService');
 router.post('/api/1.0/hoaxes', async (req, res, next) => {
   if (req.authenticatedUser) {
     await HoaxService.save(req.body);
-    return res.send();
+    return res.send({ message: req.t('hoax_submit_success') });
   }
 
   next(new AuthenticationException('unauthorized_hoax_submit'));
