ce7dc5a46edef46d945cae77676c5bbf1c62bc01
Gildas Montcho <gmontcho@gmail.com>
2022-11-04 09:35:10 +0100

    feat(hoax-user-relationship): update the migration file (add or remove userId column)

--- database/migrations/20221104082618-hoax-user-relationship.js
+++ database/migrations/20221104082618-hoax-user-relationship.js
@@ -3,20 +3,17 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('hoaxes', 'userId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id'
+      },
+      onDelete: 'cascade'
+    });
   },
 
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.deleteColumn('hoaxes', 'userId');
   }
 };
