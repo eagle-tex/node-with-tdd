d390ce88f568f3f25ebeadbf66a8b38d192bc8c6
Gildas Montcho <gmontcho@gmail.com>
2022-11-04 09:53:05 +0100

    test(HoaxListing): add #214 'returns page object as response body'

--- __tests__/HoaxListing.spec.js
+++ __tests__/HoaxListing.spec.js
@@ -12,4 +12,14 @@ describe('Listing All Hoaxes', () => {
     const response = await getHoaxes();
     expect(response.status).toBe(200);
   });
+
+  it('returns page object as response body', async () => {
+    const response = await getHoaxes();
+    expect(response.body).toEqual({
+      content: [],
+      page: 0,
+      size: 10,
+      totalPages: 0
+    });
+  });
 });
