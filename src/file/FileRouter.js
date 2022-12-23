ea2109e19d7aa19fe395cad074807dd56de34443
Gildas Montcho <gmontcho@gmail.com>
2022-12-23 12:54:12 +0100

    feat(FileRouter): pass #244 -> use multer's `single` function as a middleware

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const FileService = require('./FileService');
+const multer = require('multer');
 
-router.post('/api/1.0/hoaxes/attachments', async (req, res) => {
-  await FileService.saveAttachment();
-  res.send();
-});
+const upload = multer();
+
+router.post(
+  '/api/1.0/hoaxes/attachments',
+  upload.single('file'),
+  async (req, res) => {
+    await FileService.saveAttachment(req.file);
+    res.send();
+  }
+);
 
 module.exports = router;
