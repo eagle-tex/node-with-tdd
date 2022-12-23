3350354b3a48656ee78aa1f3f8ed81dee3bed6bd
Gildas Montcho <gmontcho@gmail.com>
2022-12-23 23:18:50 +0100

    feat(FileRouter): pass #260 -> setup multer, save as `upload` & update rte handler

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -3,15 +3,19 @@ const router = express.Router();
 const FileService = require('./FileService');
 const multer = require('multer');
 
-const upload = multer();
+const FIVE_MB = 5 * 1024 * 1024;
 
-router.post(
-  '/api/1.0/hoaxes/attachments',
-  upload.single('file'),
-  async (req, res) => {
+const upload = multer({ limits: { fileSize: FIVE_MB } }).single('file');
+
+router.post('/api/1.0/hoaxes/attachments', (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      return res.status(400).send();
+    }
+
     await FileService.saveAttachment(req.file);
     res.send();
-  }
-);
+  });
+});
 
 module.exports = router;
