bbe229921d3d5530be8763e11e05d4678c3d55fc
Gildas Montcho <gmontcho@gmail.com>
2022-12-23 12:12:01 +0100

    feat(FileRouter): pass #243 -> create a file attachment in route handler

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const router = express.Router();
+const FileService = require('./FileService');
 
-router.post('/api/1.0/hoaxes/attachments', (req, res) => {
+router.post('/api/1.0/hoaxes/attachments', async (req, res) => {
+  await FileService.saveAttachment();
   res.send();
 });
 
