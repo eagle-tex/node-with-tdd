e21068a6da01859992896cdcb016c53affa64272
Gildas Montcho <gmontcho@gmail.com>
2022-12-23 11:06:20 +0100

    feat(FileService): pass #241 -> create attachmentFolder if it doesn't exist

--- src/file/FileService.js
+++ src/file/FileService.js
@@ -5,8 +5,9 @@ const FileType = require('file-type');
 
 const { randomString } = require('../shared/generator');
 
-const { uploadDir, profileDir } = config;
+const { uploadDir, profileDir, attachmentDir } = config;
 const profileFolder = path.join('.', uploadDir, profileDir);
+const attachmentFolder = path.join('.', uploadDir, attachmentDir);
 
 const createFolders = () => {
   if (!fs.existsSync(uploadDir)) {
@@ -15,6 +16,10 @@ const createFolders = () => {
 
   if (!fs.existsSync(profileFolder)) {
     fs.mkdirSync(profileFolder);
+  }
+
+  if (!fs.existsSync(attachmentFolder)) {
+    fs.mkdirSync(attachmentFolder);
   }
 };
 
