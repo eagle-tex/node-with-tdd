6f590aadbc4b31e79d9c97f9a3b5c715798bef6c
Gildas Montcho <gmontcho@gmail.com>
2022-12-24 22:35:11 +0100

    test(FileService): add #270 remove 24h old file w/ attchmt if not used in hoax (desc. block)

--- __tests__/FileService.spec.js
+++ __tests__/FileService.spec.js
@@ -1,9 +1,13 @@
 const FileService = require('../src/file/FileService');
+const FileAttachment = require('../src/file/FileAttachment');
+const Hoax = require('../src/hoax/Hoax');
 const fs = require('fs');
 const path = require('path');
 const config = require('config');
 
 const { uploadDir, profileDir, attachmentDir } = config;
+const profileFolder = path.join('.', uploadDir, profileDir);
+const attachmentFolder = path.join('.', uploadDir, attachmentDir);
 
 describe('createFolders', () => {
   it('creates upload folder', () => {
@@ -14,15 +18,34 @@ describe('createFolders', () => {
 
   it('creates profile folder under upload folder', () => {
     FileService.createFolders();
-    const profileFolder = path.join('.', uploadDir, profileDir);
 
     expect(fs.existsSync(profileFolder)).toBe(true);
   });
 
   it('creates attachments folder under upload folder', () => {
     FileService.createFolders();
-    const attachmentFolder = path.join('.', uploadDir, attachmentDir);
 
     expect(fs.existsSync(attachmentFolder)).toBe(true);
+  });
+});
+
+describe('Scheduled Unused File Cleanup', () => {
+  it('removes the 24 hours old file with attachment entry if not used in a hoax', async () => {
+    const testFile = path.join('.', '__tests__', 'resources', 'test-png.png');
+    const targetPath = path.join(attachmentFolder, 'test-file1');
+    fs.copyFileSync(testFile, targetPath);
+    const uploadDate = new Date(Date.now() - (24 * 60 * 60 * 1000 + 1));
+
+    const attachment = await FileAttachment.create({
+      filename: 'test-file1',
+      uploadDate: uploadDate
+    });
+    await FileService.removeUnusedAttachments();
+    const attachmentAfterRemove = await FileAttachment.findOne({
+      where: { id: attachment.id }
+    });
+
+    expect(attachmentAfterRemove).toBeNull();
+    expect(fs.existsSync(targetPath)).toBe(false);
   });
 });
