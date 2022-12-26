8cd2bf9df368446f4f21eff0411c7cbc44f3fc16
Gildas Montcho <gmontcho@gmail.com>
2022-12-26 13:22:58 +0100

    feat(UserService): pass #282 -> update deleteUser func. to delete profile img (breaks other tests)

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -108,7 +108,9 @@ const updateUser = async (id, updatedBody) => {
 };
 
 const deleteUser = async (id) => {
-  await User.destroy({ where: { id: id } });
+  const user = await User.findOne({ where: { id: id } });
+  await FileService.deleteProfileImage(user.image);
+  await user.destroy();
 };
 
 const passwordResetRequest = async (email) => {
