7ece1e2d81c68bd999506f725e5e146946c7832d
Gildas Montcho <gmontcho@gmail.com>
2022-12-26 12:44:25 +0100

    feat(migrations/): update migration script for better constraints handling

--- database/migrations/20221226095838-file-attachment-cascade.js
+++ database/migrations/20221226095838-file-attachment-cascade.js
@@ -2,24 +2,70 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('fileAttachments', 'hoaxId', {
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'hoaxes',
-        key: 'id'
-      },
-      onDelete: 'cascade'
-    });
+  async up(queryInterface, _Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const constraints = await queryInterface.getForeignKeysForTables([
+        'fileAttachments'
+      ]);
+      for (let i = 0; i < constraints.fileAttachments.length; i++) {
+        const constraintName = constraints.fileAttachments[i];
+        if (constraintName.includes('hoaxId')) {
+          await queryInterface.removeConstraint(
+            'fileAttachments',
+            constraintName,
+            { transaction }
+          );
+        }
+      }
+
+      await queryInterface.addConstraint('fileAttachments', {
+        fields: ['hoaxId'],
+        type: 'foreign key',
+        references: {
+          table: 'hoaxes',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        transaction
+      });
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+    }
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('fileAttachments', 'hoaxId', {
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'hoaxes',
-        key: 'id'
+  async down(queryInterface, _Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const constraints = await queryInterface.getForeignKeysForTables([
+        'fileAttachments'
+      ]);
+      for (let i = 0; i < constraints.fileAttachments.length; i++) {
+        const constraintName = constraints.fileAttachments[i];
+        if (constraintName.includes('hoaxId')) {
+          await queryInterface.removeConstraint(
+            'fileAttachments',
+            constraintName,
+            { transaction }
+          );
+        }
       }
-    });
+
+      await queryInterface.addConstraint('fileAttachments', {
+        fields: ['hoaxId'],
+        type: 'foreign key',
+        references: {
+          table: 'hoaxes',
+          field: 'id'
+        },
+        transaction
+      });
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+    }
   }
 };
